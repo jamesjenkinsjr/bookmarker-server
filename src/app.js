6627f139178edbe4e39bed5a93b535a1b1f7c73e
jamesjenkinsjr <jamesjenkinsjr@icloud.com>
2019-09-27 12:33:04 -0400

    install validator for urls, implement POST

--- src/app.js
+++ src/app.js
@@ -16,6 +16,7 @@ const morganOption = (NODE_ENV === 'production')
 app.use(morgan(morganOption));
 app.use(helmet());
 app.use(cors());
+app.use(express.json());
 
 // error handler
 app.use((error, req, res, next) => {
--- src/bookmark-router.js
+++ src/bookmark-router.js
@@ -1,12 +1,43 @@
 const express = require('express');
-const store = require('./store');
+const bookmarks = require('./store');
+const logger = require('./logger');
+const uuid = require('uuid/v4');
+const validator = require('validator');
 
 const bookmarkRouter = express.Router();
 
 bookmarkRouter
   .route('/')
   .get((req, res) => {
-    res.status(200).json(store);
+    return res.status(200).json(bookmarks);
+  })
+  .post((req, res) => {
+    !req.body && res.status(400).json('Invalid data');
+    const { title, url, content = '', rating = 5,  } = req.body;
+    if(!title) {
+      logger.error('title is required');
+      return res.status(400).json('invalid data');
+    } 
+    if(!url) {
+      logger.error('url is required');
+      return res.status(400).json('invalid data');
+    }
+    if(!validator.isURL(url)) {
+      logger.error('url is not in proper format');
+      return res.status(400).json('invalid data');
+    }
+
+    const newBookmark = {
+      id: uuid(),
+      title,
+      url,
+      content,
+      rating
+    };
+
+    bookmarks.push(newBookmark);
+    logger.info(`Bookmark with id ${newBookmark.id} created`);
+    return res.status(201).json(`Bookmark with id ${newBookmark.id} created`);
   });
 
 module.exports = bookmarkRouter;
