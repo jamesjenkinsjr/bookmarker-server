e24f94010e3904c8892ab3b484cf83cbaf1af26e
jamesjenkinsjr <jamesjenkinsjr@icloud.com>
2019-09-30 14:52:34 -0400

    add validation for rating being numeric

--- src/bookmark-router.js
+++ src/bookmark-router.js
@@ -13,20 +13,24 @@ bookmarkRouter
   })
   .post((req, res) => {
     if(!req.body) {
-      return res.status(400).json('Invalid data');
+      return res.status(400).send('invalid data');
     }
     const { title, url, content = '', rating = 5,  } = req.body;
     if(!title) {
       logger.error('title is required');
-      return res.status(400).json('invalid data');
+      return res.status(400).json({error: 'invalid data - title and url are required'});
     } 
     if(!url) {
       logger.error('url is required');
-      return res.status(400).json('invalid data');
+      return res.status(400).json({error: 'invalid data - title and url are required'});
     }
     if(!validator.isURL(url)) {
       logger.error('url is not in proper format');
-      return res.status(400).json('invalid data');
+      return res.status(400).json({error: 'invalid data - url must have proper format'});
+    }
+    if(!parseInt(rating)) {
+      logger.error('rating is not valid');
+      return res.status(400).json({error: 'invalid data - rating must be a number'});
     }
 
     const newBookmark = {
@@ -39,7 +43,7 @@ bookmarkRouter
 
     bookmarks.push(newBookmark);
     logger.info(`Bookmark with id ${newBookmark.id} created`);
-    return res.status(201).json(`Bookmark with id ${newBookmark.id} created`);
+    return res.status(201).json({message: `Bookmark with id ${newBookmark.id} created`});
   });
 
 bookmarkRouter
@@ -49,11 +53,11 @@ bookmarkRouter
     const bookmark = bookmarks.find(bookmark => bookmark.id === id);
     if(!id) {
       logger.error('id is required');
-      return res.status(400).json('invalid data');
+      return res.status(400).json({error: 'invalid data - check that your id is valid'});
     }
     if(!bookmark) {
       logger.error(`id ${id} not found`);
-      return res.status(400).json('invalid data');
+      return res.status(400).json({error: 'invalid data - check that your id is valid'});
     }
     return res.status(200).json(bookmark);
   })
@@ -63,14 +67,14 @@ bookmarkRouter
     const bookmarkID = bookmarks.findIndex(bookmark => bookmark.id === id);
     if(!id) {
       logger.error('id is required');
-      return res.status(400).json('invalid data');
+      return res.status(400).json({error: 'invalid data - check that your id is valid'});
     }
     if(!bookmark) {
       logger.error(`id ${id} not found`);
-      return res.status(400).json('invalid data');
+      return res.status(400).json({error: 'invalid data - check that your id is valid'});
     }
     bookmarks.splice(bookmarkID, 1);
-    return res.status(204).json('Deleted');
+    return res.status(204).end();
   });
 
 module.exports = bookmarkRouter;
